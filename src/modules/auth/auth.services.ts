79e52938c13bb3e98f3821a5f6ff4eeb43bb67f3
MD-ABDULLAH24 <abdullah.codingmind@gmail.com>
2025-12-31 21:04:47 +0600

    error handling

--- src/modules/auth/auth.services.ts
+++ src/modules/auth/auth.services.ts
@@ -49,7 +49,7 @@ const loginUser = async (email: string, password: string) => {
 
   const token = jwt.sign(
     { user: user.name, email: user.email, role: user.role },
-    config.secrete as string,
+    config.secret as string,
     {
       expiresIn: "7d",
     }
